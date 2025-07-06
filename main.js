2530bd5e6054994f92ad65e2fd88272fa0d7b797
katboz <136274505+katboz@users.noreply.github.com>
2025-07-06 23:20:43 +0000

    Looping  through every character in the json.results array

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 "use strict";
 fetch('https://rickandmortyapi.com/api/character').then((r) => r.json()).then((json) => {
-    console.log(json);
+    // fetching Rick and Morty character data, I am taking name, species, gender, image and episode. The line bellow loops through every character in the json.results array.
+    json.results.forEach((character) => {
+        console.log(`Name: ${character.name}`);
+        console.log(`Species: ${character.species}`);
+        console.log(`Gender: ${character.gender}`);
+        console.log(`Image: ${character.image}`);
+        console.log(`Episodes: ${character.episode.length}`);
+    });
 });
+// for each loap I am fetching the character's characteristics.
--- main.ts
+++ main.ts
@@ -25,23 +25,14 @@ type Character = {
   url: string,
 }
 fetch('https://rickandmortyapi.com/api/character').then((r) => r.json()).then((json:{info:Info, results:Character[] }) => {
-
-console.log(json);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// fetching Rick and Morty character data, I am taking name, species, gender, image and episode. The line bellow loops through every character in the json.results array.
+ json.results.forEach((character: Character) => {
+      console.log(`Name: ${character.name}`);
+      console.log(`Species: ${character.species}`);
+      console.log(`Gender: ${character.gender}`);
+      console.log(`Image: ${character.image}`);
+      console.log(`Episodes: ${character.episode.length}`);
+    });
   });
+
+  // for each loap I am fetching the character's characteristics.
